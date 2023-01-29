160de41bbc8e85fa8a318681c1b4e2bd2353c8f9
lakeisha1514 <lwill162@depaul.edu>
2023-01-28 21:32:34 -0600

    Adding Course

--- students/admin.py
+++ students/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from .models import Student
+from .models import Student, Course
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Course)
