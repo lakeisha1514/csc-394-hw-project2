7e6a3198e8fbe9468f041cea5cb3d0fe21af9f35
lakeisha1514 <lwill162@depaul.edu>
2023-01-30 16:34:46 -0600

    HW10

--- students/views.py
+++ students/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+@login_required
+def index(request):
+
+    return render(request, "index.html")
